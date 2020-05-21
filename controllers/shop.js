01da1a4c632d95190e8e291b780ae25bf363ab6f
hansa-lallu <hansalallu@gmail.com>
2020-05-21 06:31:53 +0100

    extracting dynamic params - id

--- controllers/shop.js
+++ controllers/shop.js
@@ -20,6 +20,11 @@ exports.getIndex = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  res.redirect('/');
+}
+
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     path: '/cart',
