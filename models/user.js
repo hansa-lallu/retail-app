868162da21f5128ec2036bfe5df8c80e7d65828e
hansa-lallu <hansalallu@gmail.com>
2020-05-31 02:29:47 +0100

    Creating a user

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongodb = require('mongodb');
+const getdb = require('../utl/database').getDb;
+
+const ObjectId = mongodb.ObjectId;
+
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
+
+  save() {
+    const db = getDb;
+    return db.collection('users').insertOne(this);
+  }
+  
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection('users'
+      .findOne({_id: new ObjectId(userId)
+    }))
+  }
+    
+}
+
+module.exports = User;
