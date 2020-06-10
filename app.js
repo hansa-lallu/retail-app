7ddb96702702275712acf13738dc0a37430fd28f
hansa-lallu <hansalallu@gmail.com>
2020-06-10 08:51:07 +0100

    using mongodb to store a session

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const store = new MongoDBStore({
+  uri: process.env.DB_HOST,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs'); 
 app.set('views', 'views');
@@ -20,7 +25,14 @@ const authRoutes = require('./routes/auth')
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'my secret', resave: false, saveUninitialized: false}))
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);  
 
 app.use((req, res, next) => {
   User.findById('5edc622d537908cc12bd3fe1')
--- controllers/auth.js
+++ controllers/auth.js
@@ -12,6 +12,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly')
+  req.session.isLoggedIn = true
   res.redirect('/');
 };
